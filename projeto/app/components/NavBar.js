5c86d04aa5f991be3d14f80a630552a687a9e747
Felipecpo1 <felipeppt1@gmail.com>
2023-10-06 00:07:41 -0300

    Config Design

---
+++ projeto/app/components/NavBar.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+import styles from './NavBar.module.css'
+
+export default function NavBar() {
+  return (
+    <div className={styles.display}>
+      <ul>
+        <li className=' ml-5'><Link href=''><i className=" text-xl fa-solid fa-house"> </i> Início</Link></li>
+        <li><Link href=''><i className=" text-xl pl-2 fa-solid fa-user"></i> Sobre Mim</Link></li>
+        <li><Link href=''> <i className=" text-xl fa-solid fa-camera-retro"></i> Fotos</Link></li>
+        <li> <Link href=''> <i className=" text-xl fa-solid fa-comment"></i> Contato</Link></li>
+      </ul>
+    </div>
+  )
+}
---
+++ projeto/app/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'/>
+    </div>
+  )
+}
