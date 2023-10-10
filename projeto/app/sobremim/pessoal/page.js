b308a8121a7a2aa258e43707710655ef1853b19b
Felipecpo1 <felipeppt1@gmail.com>
2023-10-10 00:07:27 -0300

    SobreMim design

--- projeto/app/sobremim/pessoal/page.js
+++ projeto/app/sobremim/pessoal/page.js
@@ -1,26 +1,28 @@
 'use client'
 import React from 'react'
+import {Navigation, Pagination, A11y} from 'swiper/modules'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import 'swiper/css/bundle';
 import Pessoal1 from './components/Pessoal1';
 import Pessoal2 from './components/Pessoal2';
 import Pessoal3 from './components/Pessoal3';
 import styles from '../../components/Artigo.module.css'
-import { Navigation, Pagination, A11y } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
 
-export default () => {
+
+export default function page() {
   return (
     <div className={styles.display}>
-    <Swiper
+      <Swiper
       modules={[Navigation, Pagination, A11y]}
-      spaceBetween={50}
-      slidesPerView={1}
+      slidesPerView={[1]}
+      spaceBetween={20}
+      pagination={{clickable: true}}
       navigation
-      pagination={{ clickable: true }}
-    >
-      <SwiperSlide><Pessoal1/></SwiperSlide>
-      <SwiperSlide><Pessoal2/></SwiperSlide>
-      <SwiperSlide><Pessoal3/></SwiperSlide>
-    </Swiper>
+      >
+        <SwiperSlide><Pessoal1/></SwiperSlide>
+        <SwiperSlide><Pessoal2/></SwiperSlide>
+        <SwiperSlide><Pessoal3/></SwiperSlide>
+      </Swiper>
     </div>
-  );
-};
+  )
+}
