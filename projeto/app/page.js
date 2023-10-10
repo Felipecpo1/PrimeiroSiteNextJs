1d3d0ae40e51bea917dd2c60dca274484f40cd13
Felipecpo1 <felipeppt1@gmail.com>
2023-10-10 14:46:58 -0300

    Background

--- projeto/app/page.js
+++ projeto/app/page.js
@@ -2,16 +2,18 @@ import styles from './components/Artigo.module.css'
 
 export default function Home() {
   return (
-    <div className=' mt-16'>
+    <div className=' mt-36'>
+      <div>
       <section className={styles.display}>
         <article>
-          <h1><i class="fa-solid fa-star"></i> Apresentando meu trabalho</h1>
+          <h1><i className="fa-solid fa-star"></i> Apresentando meu trabalho</h1>
           <p>Olá! Meu nome é Felipe César!</p> <p>Nesse projeto, vocês encontrarão um pouco sobre meu processo de aprendizado.</p>
           <p>Na parte superior, o menu te fará navegar pelo site e encontrar informações pessoais e profissionais sobre mim, além de fotos e meios para entrar em contato. </p>
           <p>Esse projeto também inclui o desenvolvimento de aplicações em Python, e a tentativa de incorporá-las ao site.</p>
           <p>No momento, me dedico ao básico do projeto.</p>
         </article>
       </section>
+      </div>
     </div>
   )
 }
--- projeto/app/sobremim/pessoal/components/Pessoal3.js
+++ projeto/app/sobremim/pessoal/components/Pessoal3.js
@@ -6,7 +6,7 @@ export default function Pessoal3() {
       <section>
         <article>
           <h1><i className="fa-solid fa-graduation-cap"></i> Vida acadêmica:</h1>
-          <p>Nesse ano de 2023, resolvi destrancar a matrícula de Análise e Desenvolvimento de Sistemas, na Estácio, onde havia cursado dois períodos em 2021. Essa foi a segunda faculdade que precisei trancar, por questões financeiras. A outra foi no na Tijuca, la no Rio, quando tentei cursar Design Gráfico. Mas agora vai! Até meios do ano que vem, eu finalmente estarei graduado numa das áreas que eu mais curto em TI!!!</p>
+          <p>Nesse ano de 2023, resolvi destrancar a matrícula de Análise e Desenvolvimento de Sistemas, na Estácio, onde havia cursado dois períodos em 2021. Essa foi a segunda faculdade que precisei trancar, por questões financeiras. A outra foi no na Tijuca, la no Rio, quando tentei cursar Design Gráfico. Mas agora vai! Até meio do ano que vem, eu finalmente estarei graduado numa das áreas que eu mais curto em TI!!!</p>
           <p>E é isso aí! Quer saber mais sobre mim, clique no botão de informações profissionais!</p>
         </article>
       </section>
